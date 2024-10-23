d63adea2e4c724d1c51cb76f1185ec71d4d8665e
Dario Ramirez <dariorama60@gmail.com>
2024-10-22 21:47:28 -0300

    Subiendo archivos al file sistem falta renombrarlos

---
+++ src/files/files.controller.ts
@@ -0,0 +1,29 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, UploadedFile, UseInterceptors, BadRequestException } from '@nestjs/common';
+import { FilesService } from './files.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { fileFilter } from './helpers/fileFilter';
+import { diskStorage } from 'multer';
+
+
+
+@Controller('files')
+export class FilesController {
+  constructor(private readonly filesService: FilesService) {}
+
+  @Post('product')
+  @UseInterceptors( FileInterceptor('file',{
+    fileFilter: fileFilter,
+    limits: { fileSize: 100000 },
+    storage: diskStorage({
+      destination: './static/uploads'
+    })
+  }) )
+  uploadProductImage( @UploadedFile() file: Express.Multer.File){
+
+    if( !file ){
+      throw new BadRequestException('Make sure that file is an image')
+    }
+    return {fileName: file.originalname};
+  }
+ 
+}
