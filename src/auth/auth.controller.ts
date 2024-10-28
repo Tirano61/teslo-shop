76adc41bbe5d7a6851ff6e561a8711a8cb6cca05
Dario Ramirez <dariorama60@gmail.com>
2024-10-28 18:02:15 -0300

    comienzo del custom guard y custom decorator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,13 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req } from '@nestjs/common';
+import { Controller, Get, Post, Body, UseGuards, Headers, SetMetadata } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { LoginUserDto } from './dto/loogin-user.dto';
 import { AuthGuard } from '@nestjs/passport';
 import { GetUser } from './decorators/get-user.decorator';
 import { User } from './entities/user.entity';
+import { RawHeaaders } from './decorators/raw-headers.decorator';
+import { IncomingHttpHeaders } from 'http';
+import { UserRoleGuard } from './guard/user-role/user-role.guard';
 
 
 @Controller('auth')
@@ -24,16 +27,37 @@ export class AuthController {
   @Get('private')
   @UseGuards( AuthGuard() )
   testingPrivateRout(
-    @GetUser() user: User
+    @GetUser() user: User,
+    @GetUser('email') userEmail: string,
+    @RawHeaaders() rawHeader: string[],
+    @Headers() headers: IncomingHttpHeaders
     //@Req() request: Express.Request
   ){
-  
+    
     //console.log({user: request.user});
     return {
       ok: true,
       msg: 'Hola mundo private',
-      user
+      user,
+      userEmail,
+      rawHeader,
+      headers
+
     }
   }
+
+  //! Hacer un decorador para validar los roles (Practica)
+  @Get('private2')
+  @SetMetadata('roles', [ 'admin', 'super-user'])
+  @UseGuards( AuthGuard(), UserRoleGuard )
+  privateRout2(
+    @GetUser() user: User,
+  ){
+    return {
+      ok: true,
+      user,
+    }
+  }
+
 
 }
