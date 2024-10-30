f183a19637bc56d8fa0a6ef80700f0e23728d585
Dario Ramirez <dariorama60@gmail.com>
2024-10-29 22:25:37 -0300

    Decorador compuestopor varios decoradores, y auth en otros modulos

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { SeedService } from './seed.service';
 import { SeedController } from './seed.controller';
 import { ProductsModule } from 'src/products/products.module';
-import { ProductsService } from 'src/products/products.service';
+import { AuthModule } from 'src/auth/auth.module';
+
 
 @Module({
   controllers: [SeedController],
   providers: [SeedService, ],
-  imports: [ ProductsModule ]
+  imports: [ ProductsModule, AuthModule ]
 
 })
 export class SeedModule {}
