6adb48bd4af2cd57e159a72e113e4ad6d5cb2d1a
Dario Ramirez <dariorama60@gmail.com>
2024-11-05 19:59:08 -0300

    Estado de la conexion y lista de clientes conectados

--- src/message-ws/message-ws.gateway.ts
+++ src/message-ws/message-ws.gateway.ts
@@ -1,21 +1,26 @@
-import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway } from '@nestjs/websockets';
+import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { MessageWsService } from './message-ws.service';
-import { Socket } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway({ cors: true })
 export class MessageWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  
+  @WebSocketServer() wss: Server
+
   constructor(
     private readonly messageWsService: MessageWsService
   ) {}
 
-  handleDisconnect(client: any) {
-    console.log( 'Client disconnect', client.id );
-  }
-  
   handleConnection( client: Socket ) {
-    console.log( 'Client connected', client.id );
-    
+    this.messageWsService.registerClient( client );
+    this.wss.emit('clients-updated', this.messageWsService.getConnectedClients())
+    //console.log({ conectados: this.messageWsService.getConnectedClients()});
   }
 
+  handleDisconnect(client: any) {
+    this.messageWsService.removeClient( client.id );
+    //console.log({ conectados: this.messageWsService.getConnectedClients()});
+  }
+  
 
 }
