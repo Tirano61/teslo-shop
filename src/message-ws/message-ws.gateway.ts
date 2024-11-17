3fbf1cf92cb1e809d1473837d5c4f45852085bf0
Dario Ramirez <dariorama60@gmail.com>
2024-11-16 21:55:45 -0300

    Recibir mensajes de los clientes, varias formas de emitir desde el server

--- src/message-ws/message-ws.gateway.ts
+++ src/message-ws/message-ws.gateway.ts
@@ -1,6 +1,8 @@
-import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { MessageWsService } from './message-ws.service';
 import { Server, Socket } from 'socket.io';
+import { PartialType } from '@nestjs/swagger';
+import { NewMessageDTo } from './dtos/new-mwssage.dto';
 
 @WebSocketGateway({ cors: true })
 export class MessageWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
@@ -21,6 +23,29 @@ export class MessageWsGateway implements OnGatewayConnection, OnGatewayDisconnec
     this.messageWsService.removeClient( client.id );
     //console.log({ conectados: this.messageWsService.getConnectedClients()});
   }
+
+  // message-from-client
+  @SubscribeMessage('message-from-client')
+  onMessageFromClient( client: Socket, payload: NewMessageDTo ) {
+    
+    //! Emite unicamente al cliente
+    // client.emit('message-from-server', {
+    //   fullName: 'Soy yo',
+    //   message: payload.message || 'no-message'
+    // });
+    //! Emitir a todos Menos al cliente inicial
+    // client.broadcast.emit('message-from-server', {
+    //   fullName: 'Soy yo',
+    //   message: payload.message || 'no-message'
+    // });
+    //! Emitir a todos incluso el cliente que manda
+    this.wss.emit('message-from-server', {
+        fullName: 'Soy yo',
+        message: payload.message || 'no-message'
+    });
+
+  }
+  
   
 
 }
